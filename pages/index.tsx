5c4b1337b4a87020ff18163c526f492616caf474
Fabio J Raminhuk <70978375+raminhuk@users.noreply.github.com>
2024-10-23 21:34:09 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,40 +4,10 @@ import Head from 'next/head'
 
 const intervals = [
   {
-    id: '1m',
-    name: 'Every Minute',
-    cron: '* * * * *',
-  },
-  {
-    id: '10m',
-    name: 'Every 10 mins',
-    cron: '*/10 * * * *',
-  },
-  {
-    id: '1h',
-    name: 'Every Hour',
-    cron: '0 * * * *',
-  },
-  {
-    id: '12h',
-    name: 'Every 12 hours',
-    cron: '0 */12 * * *',
-  },
-  {
     id: '1d',
     name: 'Every Day',
     cron: '0 0 * * *',
-  },
-  {
-    id: '1w',
-    name: 'Every Week',
-    cron: '0 0 * * 0',
-  },
-  {
-    id: '1mo',
-    name: 'Every Month',
-    cron: '0 0 1 * *',
-  },
+  }
 ]
 
 const image = 'https://cron-template.vercel.app/thumbnail.png'
